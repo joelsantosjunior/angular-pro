8cde7c190a78c12b54c19dbfcb8bdda1f1dd0864
joelsantosjunior <joelsantosjunior@outlook.com>
2021-07-23 01:43:50 -0300

    testes de unidade e cobertura de código

--- projects/directives/src/app/app.component.spec.ts
+++ projects/directives/src/app/app.component.spec.ts
@@ -1,35 +1,60 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CreditCardDirective } from './directives/credit-card.directive';
+import { TooltipDirective } from './directives/tooltip.directive';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        CreditCardDirective,
+        TooltipDirective
       ],
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'directives'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('directives');
+    expect(component.title).toEqual('directives');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('directives app is running!');
+    expect(compiled.querySelector('.title')?.textContent).toContain('Credit Card Details');
+  });
+
+  it('should render the credit card label', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelectorAll('.card__label')[0]?.textContent).toContain('Credit Card Number');
+  });
+
+  it('should render the security code label', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelectorAll('.card__label')[1]?.textContent).toContain('Enter your security code');
+  });
+
+  it('should render the submit button', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const buttons = compiled.querySelectorAll('button');
+
+    expect(buttons).toHaveSize(1);
+    expect(buttons[0].textContent).toEqual('Proceed Checkout');
   });
 });
